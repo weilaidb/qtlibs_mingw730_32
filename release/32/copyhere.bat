773bf2eee161d658e28dff5d6b9995504cbd98a5
yjduoduo <wxjlmr@126.com>
2020-07-15 08:07:41 +0800

    添加配置文件

--- release/32/copyhere.bat
+++ release/32/copyhere.bat
@@ -1,2 +1,4 @@
+RMDIR D:\E\QtExamplenew\qtlibs_mingw730_32\release\32\reg
+
 COPY /y D:\E\QtExamplenew\codetools\build-codesets-Desktop_Qt_5_14_2_MinGW_32_bit-Release\release\codesets.exe   D:\E\QtExamplenew\qtlibs_mingw730_32\release\32
-COPY /y D:\E\QtExamplenew\codetools\build-codesets-Desktop_Qt_5_14_2_MinGW_32_bit-Release\reg               D:\E\QtExamplenew\qtlibs_mingw730_32\release\32\reg
+xcopy /f D:\E\QtExamplenew\codetools\build-codesets-Desktop_Qt_5_14_2_MinGW_32_bit-Release\reg              D:\E\QtExamplenew\qtlibs_mingw730_32\release\32\reg
