e1e1015055e05ae2396e11d8ae0d3f24e6d2433f
yjduoduo <wxjlmr@126.com>
2020-07-15 08:01:07 +0800

    copy script

---
+++ release/32/copyhere.bat
@@ -0,0 +1,2 @@
+COPY /y D:\E\QtExamplenew\codetools\build-codesets-Desktop_Qt_5_14_2_MinGW_32_bit-Release\release\codesets.exe   D:\E\QtExamplenew\qtlibs_mingw730_32\release\32
+COPY /y D:\E\QtExamplenew\codetools\build-codesets-Desktop_Qt_5_14_2_MinGW_32_bit-Release\reg               D:\E\QtExamplenew\qtlibs_mingw730_32\release\32\reg
