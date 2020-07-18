2d403264d3d8799c8dc6c60fadadd5670b08149f
yjduoduo <wxjlmr@126.com>
2020-07-18 20:56:59 +0800

    删除库

--- release/32/copyhere.bat
+++
@@ -1,4 +0,0 @@
-RMDIR D:\E\QtExamplenew\qtlibs_mingw730_32\release\32\reg
-
-COPY /y D:\E\QtExamplenew\codetools\build-codesets-Desktop_Qt_5_14_2_MinGW_32_bit-Release\release\codesets.exe   D:\E\QtExamplenew\qtlibs_mingw730_32\release\32
-xcopy /f D:\E\QtExamplenew\codetools\build-codesets-Desktop_Qt_5_14_2_MinGW_32_bit-Release\reg              D:\E\QtExamplenew\qtlibs_mingw730_32\release\32\reg
