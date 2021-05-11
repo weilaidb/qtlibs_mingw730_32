455b6c157a95013ff466fb607eee7d6783550538
duoduo <wxjlmr@126.com>
2021-05-11 08:07:47 +0800

    release\64\reg\after\Tools\git\git clone -i 指定文件.txt

---
+++ update.bat
@@ -0,0 +1,6 @@
+REM REM set rsa_path=D:/keys/weilaidb_prikey.ppk
+REM set rsa_path=../../keys/weilaidb_prikey.ppk
+REM git config core.sshCommand "ssh -i %rsa_path% "
+REM git pull git@github.com:weilaidb/qtlibs_mingw730_32.git
+
+git pull
