c505301fd6617baf3484ead19ca64e7f86ea9f98
duoduo <wxjlmr@126.com>
2022-05-29 23:32:32 +0800

    add changes for    release/64/reg/tips/Python/scripts/web/test/hfpy2e-all/ch05/vsearch_webapp4.py

--- release/64/reg/tips/Python/scripts/web/test/hfpy2e-all/ch05/vsearch_webapp4.py
+++ release/64/reg/tips/Python/scripts/web/test/hfpy2e-all/ch05/vsearch_webapp4.py
@@ -8,11 +8,13 @@ def search4() -> 'html':
     """Returns the results of a call to 'search4letters' to the browser."""
     phrase = request.form['phrase']
     letters = request.form['letters']
+    sex = request.form['sex']
     results = str(search4letters(phrase, letters))
     return render_template('results.html',
                            the_title='Here are your results!',
                            the_phrase=phrase,
                            the_letters=letters,
+                           the_sex=sex,
                            the_results=results)
 
 @app.route('/entry')
@@ -22,14 +24,16 @@ def entry_page() -> 'html':
                            the_title='Welcome to search4letters on the web!',
                              the_url='/search4')
 
-@app.route('/searchjson', methods=['POST'])
+@app.route('/search4json', methods=['POST'])
 def search4json() -> 'json':
     """Returns the results of a call to 'search4letters' to the browser."""
     phrase = request.form['phrase']
     letters = request.form['letters']
+    sex = request.form['sex']
     results = str(search4letters(phrase, letters))
     return jsonify(the_phrase=phrase,
                    the_letters=letters,
+                   the_sex=sex,
                    the_results=results)
 
 @app.route('/entryjson')
@@ -37,7 +41,8 @@ def entry_json_page() -> 'html':
     """Returns the JSON entry page to browser."""
     return render_template('entry.html',
                            the_title='Welcome to search4letters JSON page',
-                           the_url='/searchjson')
+                           the_url='/search4json')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0",port=8080)
+
